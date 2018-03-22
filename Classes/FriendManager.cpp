da87ffbdf7ae8cf4bf1d5c562abe635bc55e2682
liushiyin <3252918599@qq.com>
2018-03-22 23:49:44 +0800

    好友功能

--- Classes/FriendManager.cpp
+++ Classes/FriendManager.cpp
@@ -1,7 +1,29 @@
 #include"FriendManager.h"
 #include"Commen.h"
+#include"DBDao.h"
+#include"Model.h"
 
-void FriendManager::addFriend(GameFriend fr)
+int FriendManager::addFriend(const GameFriend& fr)
 {
-	
+	for (auto var : m_friendlist)
+	{
+		if (var == fr)
+		{
+			return;
+		}
+	}
+	m_friendlist.push_back(fr);
+}
+
+int FriendManager::delFrinend(const GameFriend& fr)
+{
+	for (auto it = m_friendlist.begin(); it != m_friendlist.end(); ++it)
+	{
+		if ((*it) == fr)
+		{
+			m_friendlist.erase(it);
+			return 0;
+		}
+	}
+	return -1;
 }
--- Classes/FriendManager.h
+++ Classes/FriendManager.h
@@ -5,10 +5,16 @@
 #include<string>
 #include<list>
 
+//好友信息
 struct GameFriend
 {
 	std::string playername;
 	std::string rolename;
+	
+	bool operator==(const GameFriend& fr)
+	{
+		this->playername == fr.playername&&this->rolename == fr.rolename;
+	}
 };
 
 /*
@@ -21,7 +27,8 @@ class FriendManager
 	GET_SINGLE_OBJECT(FriendManager);
 public:
 	/*
-	*
+	* getFriendList()
+	*获取好友信息列表
 	*/
 	std::list<GameFriend>& getFriendList() { return m_friendlist; }
 
@@ -29,8 +36,17 @@ public:
 	*addFriend(GameFriend fr);
 	*添加朋友
 	*@param fr:朋友信息
+	*@return int:返回-1表示添加失败，返回0表示添加成功
 	*/
-	void addFriend(GameFriend fr);
+	int addFriend(const GameFriend& fr);
+
+	/*
+	*delFrinend(const GameFriend& fr)
+	*删除好友
+	*@param fr:好友信息
+	*@return int：返回-1表示删除失败，返回0表示删除成功
+	*/
+	int delFrinend(const GameFriend& fr);
 private:
 	std::list<GameFriend> m_friendlist;
 
