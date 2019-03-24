bf999c21a07761c002a9c841393421237a9cb9a4
lsylovews <3252918599@qq.com>
2019-03-24 16:14:25 +0800

    门派

---
+++ Classes/SectMainLayer.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef __SECT_MAIN_LAYER_H__
+#define __SECT_MAIN_LAYER_H__
+#include"PreProcess.h"
+
+class SectMainLayer :public cocos2d::Layer
+{
+	
+};
+
+#endif // !__SECT_MAIN_LAYER_H__
+
--- Classes/TalismanIcon.cpp
+++ Classes/TalismanIcon.cpp
@@ -55,5 +55,5 @@ void TalismanIcon::updateProperty()
 }
 
 TalismanIcon::~TalismanIcon()
-{
+{ 
 }
